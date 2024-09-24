5702c1d56c93918a9f6e0a95170e2e9e94c9a7e8
rajibul5603 <russelice08@gmail.com>
2024-09-24 10:14:42 +0600

    index changed;

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,18 +1,9 @@
 
 <div class="container-fluid footer marg30">
     <div class="container">
-        <div class="col-xs-12 col-sm-4 col-md-4 margin-top-20">
+    <div class="col-xs-12 col-sm-4 col-md-4 margin-top-20">
             <div class="panel-transparent">
-                <div class="footer-heading">টিউটোরিয়াল</div>
-                <div class="footer-body">
-                    <p>ডেটাবেজ মানে হচ্ছে তথ্যভান্ডার। কম্পিউটার আবিষ্কারের আগে পর্যন্ত ফাইলের স্তুপে জমা থাকতো তথ্য, এখন তথ্য সংরক্ষণ করা হয় ডেটাবেজে। ডাটাবেজ তৈরীর বিভিন্ন প্রোগ্রাম আছে। একটি ডাটাবেজ তৈরী করার পর সেটাকে প্রায়ই হালনাগাদ করার প্রয়োজন পড়ে। এই হালনাগাদের কাজ করার জন্যও বিভিন্ন ধরনের প্রোগ্রাম আছে। ডাটাবেজ ম্যানেজমেন্ট সিস্টেম বা ডিবিএমএস হলো এমন একটি পূর্ণাঙ্গ প্রোগ্রাম যা একই সাথে ডাটাবেজ তৈরী, ব্যবস্থাপনা, তত্ত্ববধান এবং ডাটা প্রসেসের কাজ করে। ডাটাবেজের সাহায্যে ডাটা প্রসেস করে আমরা ঐসব ডাটার প্রকৃতি ও বৈশিষ্ট্য সম্পর্কে জানতে পারি।  </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-4 col-md-4 margin-top-20">
-            <div class="panel-transparent">
-                <div class="footer-heading">Categories</div>
+                <div class="footer-heading">ট্যাগ </div>
                 <div class="footer-body">
                     <ul>
                         @foreach($footerCategories as $category)
@@ -27,7 +18,22 @@
 
         <div class="col-xs-12 col-sm-4 col-md-4 margin-top-20">
             <div class="panel-transparent">
-                <div class="footer-heading">Popular Articles</div>
+                <div class="footer-heading">মন্ত্রণালয়/বিভাগ </div>
+                <div class="footer-body">
+                    <ul>
+                        @foreach($footerCategories as $category)
+                            <li>
+                                <a href="{{ route('categories.show', [$category->slug, $category->id]) }}">{{ $category->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-4 col-md-4 margin-top-20">
+            <div class="panel-transparent">
+                <div class="footer-heading">জনপ্রিয় প্রস্তাব</div>
                 <div class="footer-body">
                     <ul>
                         @foreach($popularArticles as $article)
